31b8647141cf08970e656200aac94184c6cf94f0
Vain <106759694+VainV4@users.noreply.github.com>
2025-12-24 09:17:35 +0100

    Update config.lua

--- scripts/config.lua
+++ scripts/config.lua
@@ -1,1 +1,32 @@
+-- Config.lua
+local Config = {}
+
+Config.Settings = {
+	METAL_ESP = { ENABLED = false, COLOR = Color3.fromRGB(0, 170, 255) },
+	BEE_ESP   = { ENABLED = false, COLOR = Color3.fromRGB(255, 214, 6) },
+	STAR_ESP  = { ENABLED = false, COLOR = Color3.fromRGB(255, 255, 255) },
+	TREE_ESP  = { ENABLED = false, COLOR = Color3.fromRGB(255, 100, 0) },
+
+	AIM_ASSIST = {
+		ENABLED = false,
+		SMOOTHNESS = 0.15,
+		MAX_DISTANCE = 94,
+		MAX_ANGLE = 90
+	},
+
+	UI_COLOR = Color3.fromRGB(12, 12, 14),
+	KEY_UI_TOGGLE = Enum.KeyCode.RightShift,
+	KEY_AIM_TOGGLE = Enum.KeyCode.Q,
+	VISIBLE = true
+}
+
+-- Gemeinsamer ESP-State
+Config.ActiveObjects = {
+	metal = {},
+	star  = {},
+	tree  = {},
+	bee   = {}
+}
+
+return Config
 
