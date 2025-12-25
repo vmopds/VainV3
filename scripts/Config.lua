c258150c9aed2eb4d1390c54e4985f6c2b878c43
Vain <106759694+VainV4@users.noreply.github.com>
2025-12-25 10:53:31 +0100

    Update Config.lua

--- scripts/Config.lua
+++ scripts/Config.lua
@@ -1,30 +1,14 @@
-local Config = {}
-_G.Vain = _G.Vain or {}
-_G.Vain.Config = Config
-
-Config.Settings = {
-	METAL_ESP = { ENABLED = false, COLOR = Color3.fromRGB(0, 170, 255), TAG = "hidden-metal" },
-	BEE_ESP   = { ENABLED = false, COLOR = Color3.fromRGB(255, 214, 6), TAG = "bee" },
-	STAR_ESP  = { ENABLED = false, COLOR = Color3.fromRGB(255, 255, 255), TAG = "star" },
-	TREE_ESP  = { ENABLED = false, COLOR = Color3.fromRGB(255, 100, 0), TAG = "treeOrb" },
-
-	AIM_ASSIST = {
-		ENABLED = false,
-		SMOOTHNESS = 0.15,
-		MAX_DISTANCE = 94,
-		MAX_ANGLE = 90
-	},
-
-	UI_COLOR = Color3.fromRGB(12, 12, 14),
-	KEY_UI_TOGGLE = Enum.KeyCode.RightShift,
-	VISIBLE = true,
-	
-	ActiveObjects = {
-		["metal"] = {},
-    	["bee"] = {},
-    	["tree"] = {},
-    	["star"] = {}
+local Config = {
+    Settings = {
+        METAL_ESP = { ENABLED = false, COLOR = Color3.fromRGB(0, 170, 255), TAG = "hidden-metal" },
+        BEE_ESP   = { ENABLED = false, COLOR = Color3.fromRGB(255, 214, 6), TAG = "bee" },
+        STAR_ESP  = { ENABLED = false, COLOR = Color3.fromRGB(255, 255, 255), TAG = nil }, -- Special handling
+        TREE_ESP  = { ENABLED = false, COLOR = Color3.fromRGB(255, 100, 0), TAG = "treeOrb" },
+        
+        AIM_ASSIST = { ENABLED = false, SMOOTHNESS = 0.15, MAX_DISTANCE = 94, MAX_ANGLE = 90 },
+        UI_COLOR = Color3.fromRGB(12, 12, 14),
+        VISIBLE = true
+    },
+    ActiveObjects = { metal = {}, star = {}, tree = {}, bee = {} }
 }
-	}
-}
-return Config
+_G.Vain = { Config = Config }
