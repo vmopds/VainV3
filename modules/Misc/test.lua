ec4e105e656ea50a115422f889907def8aaf9bed
Vain <106759694+VainV4@users.noreply.github.com>
2025-12-25 15:00:45 +0100

    Update test.lua

--- modules/Misc/test.lua
+++ modules/Misc/test.lua
@@ -1,1 +1,18 @@
+return {
+    DefaultSettings = {
+        Enabled = true,
+        ShowBeams = true,
+        ShowDistance = true
+    },
+
+    Run = function(settings)
+        task.spawn(function()
+            while task.wait(2) do
+                if settings.Enabled then
+                    print("[Test]", settings.ShowBeams, settings.ShowDistance)
+                end
+            end
+        end)
+    end
+}
 
