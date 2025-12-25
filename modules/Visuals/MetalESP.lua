d28a799ce3b169a0366ab59a35a5505a0a9f6ddc
Vain <106759694+VainV4@users.noreply.github.com>
2025-12-25 15:00:23 +0100

    Update MetalESP.lua

--- modules/Visuals/MetalESP.lua
+++ modules/Visuals/MetalESP.lua
@@ -1,1 +1,17 @@
+return {
+    DefaultSettings = {
+        Enabled = true,
+        ShowBeams = true,
+        ShowDistance = true
+    },
 
+    Run = function(settings)
+        task.spawn(function()
+            while task.wait(2) do
+                if settings.Enabled then
+                    print("[MetalESP]", settings.ShowBeams, settings.ShowDistance)
+                end
+            end
+        end)
+    end
+}
