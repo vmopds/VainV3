84e7cea5da6a4bcd952fdaac57567ee46d9442a9
Vain <106759694+VainV4@users.noreply.github.com>
2025-12-25 15:16:12 +0100

    Update MetalESP.lua

--- modules/Visuals/MetalESP.lua
+++ modules/Visuals/MetalESP.lua
@@ -2,14 +2,15 @@ return {
     DefaultSettings = {
         Enabled = true,
         ShowBeams = true,
-        ShowDistance = true
+        ShowDistance = true,
+        Brightness = 50
     },
 
     Run = function(settings)
         task.spawn(function()
             while task.wait(2) do
                 if settings.Enabled then
-                    print("[MetalESP]", settings.ShowBeams, settings.ShowDistance)
+                    print("[MetalESP]", settings.ShowBeams, settings.ShowDistance, settings.Brightness)
                 end
             end
         end)
